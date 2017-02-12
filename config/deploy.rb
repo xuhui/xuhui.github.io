95e172b221f8f2387f0f60cbfb6af09fee3fd596
X.H <mail@zhouxuhui.com>
2017-02-12 17:48:56 +0800

    looks work, with ugly chmod 777, should use setfacl ??

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "blog.zhouxuhui.com"
+set :repository,  "_site"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :none
+set :deploy_via, :copy
+ssh_options[:port] = 26742
+set :copy_compress, :gzip
+set :use_sudo, false
+
+set :user, "ruby"
+set :deploy_to, "/blog/pages"
+
+role :web, "blog.zhouxuhui.com"                          # Your HTTP server, Apache/etc
+## role :app, "your app-server here"                          # This may be the same as your `Web` server
+## role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+## role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -23,3 +32,8 @@ role :db,  "your slave db-server here"
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+
+after "deploy:finalize_update" do
+  run "chmod -R 777 #{latest_release}"
+end
